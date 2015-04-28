643ead4c072422ea5258dee92134e9874302363f
BrayanGV <megas_css@hotmail.com>
2015-04-28 16:19:21 -0400

    Clases de Brayan
    clases iniciales

---
+++ src/general/Region.java
@@ -0,0 +1,56 @@
+package general;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Region {
+
+	private int codigo;
+	private String nombre;
+	private int numero;
+	
+	public Region(){
+	}
+	
+	public int getcodigo(){
+		return codigo;	
+	}
+	
+	public void setcodigo(int cod){
+		codigo=cod;
+	}
+	
+	public String getnombre(){
+		return nombre;
+	}
+	
+	public void setnombre(String nom){
+		nombre=nom;
+	}
+	
+	public int getnumero(){
+		return numero;
+	}
+	
+	public void setnumero(int num){
+		numero=num;
+	}
+	
+		
+	public void ingreso(){	
+	}
+	
+	public String consulta(){
+		String consultaregion="";
+		return consultaregion;
+	}
+	
+	public Object listar(){
+		object
+	}
+	
+	
+}
